79816eae12e19a2befd42ced1c1521f6e1f3c3d3
Carlos Cruz <cruz5114@gmail.com>
2017-12-07 17:02:51 -0600

    fix webpack configuration on prod

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -1,8 +1,12 @@
 const merge = require('webpack-merge');
 const webpack = require('webpack');
+const path = require('path');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+  resolveLoader: {
+    root: path.join(__dirname, 'client/node_modules'),
+  },
   plugins: [
     new webpack.DefinePlugin({
       __ENV__: JSON.stringify('production'),
