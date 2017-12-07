e64de5dfa78d10be22a428ac5635db69e3ec2848
Carlos Cruz <cruz5114@gmail.com>
2017-12-07 16:46:12 -0600

    remove webpack DedupePlugin

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -7,7 +7,6 @@ module.exports = merge(common, {
     new webpack.DefinePlugin({
       __ENV__: JSON.stringify('production'),
     }),
-    new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false,
