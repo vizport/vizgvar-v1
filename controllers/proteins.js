50c6a9400688c765a2f63f6bb49c3bb82f7fbd29
Carlos Cruz <cruz5114@gmail.com>
2017-12-11 23:01:06 -0600

    add search from server

--- controllers/proteins.js
+++ controllers/proteins.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const Fuse = require('fuse.js');
 
 const router = express.Router();
-const proteins = require('./data/vproteins.json');
+const proteins = require('./data/proteins.json');
 
-router.get('/', (req, res) => {
-  res.send(proteins);
+const options = {
+  minMatchCharLength: 3,
+  matchAllTokens: true,
+  threshold: 0,
+  distance: 1000,
+  keys: [
+    "protein",
+    "protein_id"
+  ]
+};
+
+const fuse = new Fuse(proteins, options);
+
+router.get('/search/:str', (req, res) => {
+  const result = fuse.search(req.params.str);
+  res.send(result);
 });
 
 module.exports = router;
